b7b71f013cdfcc0fab359d7d948943c47d55fb9a
Ivan Firestone <ivan.firestone@icloud.com>
2024-03-09 10:07:00 -0400

    Instrucciones y Bucles
    Instrucciones y Bucles

---
+++ EstructurasControl/Program.cs
@@ -0,0 +1,51 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+
+//Necesitamos Imprimir los numeros pares del 1 al 1000;
+
+
+
+//Loop o Bucle (FOR)
+Console.WriteLine("Bucle FOR");
+for (int i = 11; i <= 10; i++)
+{
+    if(i % 2 == 0)
+    {
+        Console.WriteLine("El numero " + i + " Es PAR");
+    } else
+    {
+        Console.WriteLine("El numero " + i + " Es IMPAR");
+    }
+}
+
+//Loop o Bucle (WHILE)
+Console.WriteLine("Bucle While");
+int y = 11;
+while (y <= 10)
+{
+    y++;
+    if (y % 2 == 0)
+    {
+        Console.WriteLine("El numero " + y + " Es PAR");
+    } else
+    {
+        Console.WriteLine("El numero " + y + " Es IMPAR");
+    }
+}
+
+//Loop o Bucle (DO WHILE)
+int x = 11;
+Console.WriteLine("Bucle DO WHILE");
+do
+{
+    if (x % 2 == 0)
+    {
+        Console.WriteLine("El numero " + x + " Es PAR");
+    }
+    else
+    {
+        Console.WriteLine("El numero " + x + " Es IMPAR");
+    }
+    x++;
+
+} while (x <= 10);
---
+++ Instrucciones/Program.cs
@@ -0,0 +1,58 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+using System.Runtime.CompilerServices;
+
+int numero1 = 20;
+int numero2 = 30;
+int numero3 = 40;
+
+
+ejecutarTodo();
+
+void ejecucion2()
+{
+    Console.WriteLine("Ejecucion 2");
+    if (((numero1 + numero2) > numero3) && numero3 < 100)
+    {
+        multiplicar();
+    }
+    else
+    {
+        sumar();
+    }
+}
+
+void ejecucion1()
+{
+    Console.WriteLine("Ejecucion 1");
+    //Voy a sumar si la afirmacion es positiva y restar si es falsa
+    if (numero1 >= numero2)
+    {
+        sumar();
+    }
+    else
+    {
+        restar();
+    }
+}
+
+void ejecutarTodo()
+{
+    ejecucion1();
+    ejecucion2();
+}
+
+ void sumar()
+{
+    Console.WriteLine("Sumando: " + numero1 + numero2);
+}
+
+void restar()
+{
+    Console.WriteLine("Restando: " + (numero2 - numero1));
+}
+
+void multiplicar()
+{
+    Console.WriteLine("Multiplicando: " + numero2 * numero3);
+}
