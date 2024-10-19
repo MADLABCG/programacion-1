625f4ad26c16fe4da2599bc5ba5fa7805f4f8c00
Ivan Firestone <ivan.firestone@icloud.com>
2024-10-19 15:42:17 -0400

    Operadores

---
+++ PruebaProgramacion1/Program.cs
@@ -0,0 +1,19 @@
+﻿const string TITULO = "Programacion 1 - Variables";
+
+Console.WriteLine(TITULO);
+
+string nombres = "Ivan Jose";
+string apellidos = "Firestone";
+string matricula = "SC-00-000001";
+string carrera = "Informatica";
+int cuatrimestres = 10;
+double costo = 230000.56;
+
+nombres = "Ivan";
+
+Console.WriteLine("Nombres y Apellidos: " + nombres + " " + apellidos);
+Console.WriteLine("Matricula: " + matricula);
+Console.WriteLine("Carrera: " + carrera);
+Console.WriteLine("Cantidad Cuatrimestres: " + cuatrimestres);
+Console.WriteLine("Costo Carrera: " + costo);
+
---
+++ operadoresp1/Program.cs
@@ -0,0 +1,26 @@
+﻿const string TITULO = "Operadores";
+Console.WriteLine(TITULO);
+
+int numero1 = 3456;
+int numero2 = 98;
+double resultado = 0;
+
+//SUMA +
+resultado = numero1 + numero2;
+Console.WriteLine("Operacion Suma: " + resultado);
+
+//RESTA -
+resultado = numero1 - numero2;
+Console.WriteLine("Operacion Resta: " + resultado);
+
+//MULTIPLICACION *
+resultado = numero1 * numero2;
+Console.WriteLine("Operacion Multiplicacion: " + resultado);
+
+//DIVISION /
+resultado = numero1 / numero2;
+Console.WriteLine("Operacion Division: " + resultado);
+
+//MODULO %
+resultado = numero1 % numero2;
+Console.WriteLine("Operacion Modulo: " +  resultado);
