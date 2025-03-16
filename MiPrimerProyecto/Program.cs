12410592bf809aa068fb52dd872e9deae1fc1f39
Ivan Firestone <ivan.firestone@icloud.com>
2025-03-16 08:28:34 -0400

    Clases Marzo 15 2025

---
+++ MiPrimerProyecto/Program.cs
@@ -0,0 +1,23 @@
+﻿Console.WriteLine("Hola cual es tu nombre ? : ");
+String nombre = Console.ReadLine();
+
+Console.WriteLine("Cual es tu apellido ? : ");
+String apellido = Console.ReadLine();
+
+Console.WriteLine("Cual es tu edad ? : ");
+String edad = Console.ReadLine();
+
+Console.WriteLine("Cual es tu direccion ? : ");
+String direccion = Console.ReadLine();
+
+Console.WriteLine("Cual es tu telefono ? : ");
+String telefono = Console.ReadLine();
+
+Console.WriteLine("Esta es tu informacion : ");
+Console.WriteLine("====================================");
+Console.WriteLine("Nombre : " + nombre);
+Console.WriteLine("Apellido : " + apellido);
+Console.WriteLine("Edad : " + edad);
+Console.WriteLine("Direccion : " + direccion);
+Console.WriteLine("Telefono : " + telefono);
+Console.WriteLine("====================================");
---
+++ MiSegundoProyecto/Program.cs
@@ -0,0 +1,33 @@
+﻿Console.WriteLine("Calculadora para Sumar 2 Numeros");
+
+int num1;
+int num2;
+
+primer_numero:
+try
+{
+    Console.WriteLine("Ingrese el primer numero: ");
+    num1 = Convert.ToInt32(Console.ReadLine());
+}
+catch (Exception)
+{
+    Console.WriteLine("Esto no es numero ");
+    goto primer_numero;
+}
+
+segundo_numero:
+try
+{
+    Console.WriteLine("Ingrese el segundo numero: ");
+    num2 = Convert.ToInt32(Console.ReadLine());
+}
+catch (Exception)
+{
+    Console.WriteLine("Esto no es numero ");
+    goto segundo_numero;
+}
+
+int resultado = num1 + num2;
+Console.WriteLine("El resultado de la suma es: " + resultado);
+Console.ReadKey();
+
